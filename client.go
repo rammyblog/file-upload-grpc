ceba37b3f13da01bd75f4df2787f53dc4436b860
Onasanya Tunde <onasanyatunde67@gmail.com>
2024-04-14 03:29:36 +0100

    fix issues

--- client.go
+++ client.go
@@ -1,5 +1,83 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	pb "github.com/rammyblog/file-upload-grpc/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"io"
+	"log"
+	"os"
+)
+
+func uploadFile(client pb.FileServiceClient, filePath string) {
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalf("%v.Upload(_) = _, %v", client, err)
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("os.Open(_) = _, %v", filePath)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("file.Close(_) = %v", err)
+		}
+	}(file)
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := file.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("file.Read(_) = _, %v", err)
+		}
+		err = stream.Send(&pb.FileUploadRequest{
+			Filename: filePath,
+			Content:  buf[:n],
+		})
+		if err != nil {
+			return
+		}
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got %v, want %v", stream, err, nil)
+	}
+	log.Printf("Upload result: %v", res)
+	log.Printf("Upload finished: %v", res.Message)
+}
+
 func main() {
 
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("could not close connection: %v", err)
+		}
+	}(conn)
+
+	client := pb.NewFileServiceClient(conn)
+
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: [upload/download] [file name]")
+		return
+	}
+	operation, filePath := os.Args[1], os.Args[2]
+
+	switch operation {
+	case "upload":
+		uploadFile(client, filePath)
+	}
 }
--- server.go
+++ server.go
@@ -1,5 +1,65 @@
 package main
 
-func main() {
+import (
+	pb "github.com/rammyblog/file-upload-grpc/protos"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	"net"
+	"os"
+)
 
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+func (s server) Upload(stream pb.FileService_UploadServer) error {
+	var fileName string
+	var fileData []byte
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			outFile, err := os.Create(fileName)
+			if err != nil {
+				return err
+			}
+			defer func(outFile *os.File) {
+				err := outFile.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(outFile)
+			if _, err := outFile.Write(fileData); err != nil {
+				return err
+			}
+			return stream.SendAndClose(&pb.FileUploadSuccess{
+				Success: true,
+				Message: "Upload success",
+			})
+		}
+		if err != nil {
+			return err
+		}
+		fileName = "file_ser.proto"
+		fileData = append(fileData, req.Content...)
+
+	}
+
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterFileServiceServer(grpcServer, new(server))
+	log.Println("Server listening at", lis.Addr())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
